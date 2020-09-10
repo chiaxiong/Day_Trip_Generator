3e170a547cd425795357ce6339ba91e5f93e4b5c
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-09-09 21:06:49 -0500

    added index.thml and index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+let destination = ['Japan', 'Germany', 'Paris', 'Cancoon', 'Korean', 'England'];
+let result = ' ';
+
+for(let i = 0; i < destination.length; i++){
+    let randomNumber = Math.random(i) * 6;
+} console.log(destination);
